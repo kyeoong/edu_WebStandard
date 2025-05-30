42d13da10d142c90f87c2ca6d5eff502bd55a78c
kyeoong <nogangsss@gmail.com>
2025-05-30 17:34:29 +0900

    공부

---
+++ och16_mvc02_temp/src/main/java/control/Controller.java
@@ -0,0 +1,58 @@
+package control;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet(name = "Controller_t", urlPatterns = { "/Controller_t" })
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    protected Map<String, Object> commandMap = new HashMap<String, Object>();   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		String props = config.getInitParameter("config");
+		
+		// 파일 경로 읽어와야 함을 알아차리기
+		Properties pr = new Properties();
+		File f = null;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
